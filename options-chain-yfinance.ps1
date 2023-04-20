9afae8bb0cc85295f5dd8966204f61ba75a9f7cc
Eduardo Cavazos <wayo.cavazos@gmail.com>
2023-04-20 09:07:31 -0700

    Updates

--- options-chain-yfinance.ps1
+++ options-chain-yfinance.ps1
@@ -1,23 +1,8 @@
 
-
-
-
-
-$result = Invoke-RestMethod 'https://query1.finance.yahoo.com/v7/finance/options/SPY?date=1710460800'
-
-
-([DateTimeOffset] (Get-Date '2024-03-15')).ToUnixTimeSeconds()
-
-$symbol = 'SPY'
-$date = '2024-03-15'
-
+function percent ($a, $b) { ($b - $a) / $a }
+# ----------------------------------------------------------------------
 function get-options-chain ($symbol, $date)
 {
-    # echo ('https://query1.finance.yahoo.com/v7/finance/options/{0}?date={1}' -f $symbol, ([DateTimeOffset] (Get-Date $date)).ToUnixTimeSeconds())
-    # Invoke-RestMethod ('https://query1.finance.yahoo.com/v7/finance/options/{0}?date={1}' -f $symbol, ([DateTimeOffset] (Get-Date $date)).ToUnixTimeSeconds())
-
-    # Get-Date 2024-03-15 -UFormat %s
-
     $chain = Invoke-RestMethod ('https://query1.finance.yahoo.com/v7/finance/options/{0}?date={1}' -f $symbol, (Get-Date $date -UFormat %s))
     # ----------------------------------------------------------------------
     # mid price    
@@ -26,8 +11,10 @@ function get-options-chain ($symbol, $date)
     {        
         $val = ($row.ask + $row.bid) / 2
         
-        $row | Add-Member -MemberType NoteProperty -Name mid_price -Value $val
+        $row | Add-Member -MemberType NoteProperty -Name mid_price -Value $val -Force
     }
+    # ----------------------------------------------------------------------
+    # pct_chg_per_dol
     # ----------------------------------------------------------------------
     $prev = $chain.optionChain.result[0].options[0].puts[0]
 
@@ -46,18 +33,15 @@ function get-options-chain ($symbol, $date)
         $prev = $row
     }
     # ----------------------------------------------------------------------
+    $vol_max = ($chain.optionChain.result[0].options[0].puts | Measure-Object -Maximum volume).Maximum
 
+    $oi_max = ($chain.optionChain.result[0].options[0].puts | Measure-Object -Maximum openInterest).Maximum
     foreach ($row in $chain.optionChain.result[0].options[0].puts)
     {
-        $vol_max = ($result.optionChain.result[0].options[0].puts | Measure-Object -Maximum volume).Maximum
-
         $vol_pct = [math]::Round($row.volume / $vol_max, 6)
 
         $row | Add-Member -MemberType NoteProperty -Name vol_pct -Value $vol_pct
-
         # ----------------------------------------------------------------------
-        $oi_max = ($result.optionChain.result[0].options[0].puts | Measure-Object -Maximum openInterest).Maximum
-
         $oi_pct = [math]::Round($row.openInterest / $oi_max, 6)
 
         $row | Add-Member -MemberType NoteProperty -Name oi_pct -Value $oi_pct
@@ -66,169 +50,12 @@ function get-options-chain ($symbol, $date)
 
     $chain    
 }
-
-$result = get-options-chain SPY 2023-04-20
-
-$result = get-options-chain SPY 2024-03-15
-
-
-
-
-$result_0 = get-options-chain SPY 2023-05-19
-$result_1 = get-options-chain SPY 2023-06-16
-$result_2 = get-options-chain SPY 2024-03-15
-
-
-
-
-
-
-
-
-1710460800
-
-1710486000
-
-([DateTimeOffset] (Get-Date $date)).ToUnixTimeSeconds()
-
-
-1710460800
-
-[System.DateTimeOffset]::FromUnixTimeSeconds(1710460800).DateTime.ToString('s')
-
-
-[System.DateTimeOffset]::FromUnixTimeSeconds(1710460800).DateTime.ToString('yyyy-MM-dd')
-
-
-[System.DateTimeOffset]
-
-
-
-[System.DateTimeOffset](Get-Date 2024-03-15)
-
-[System.DateTimeOffset](Get-Date 1970-01-01)
-
-([System.DateTimeOffset](Get-Date 1970-01-01)).ToUnixTimeSeconds()
-
-
-[System.DateTimeOffset]::new(1970, 1, 1, 0, 0, 0, [timespan]::Zero)
-
-
-
-[System.DateTimeOffset]::new(1970, 1, 1, 0, 0, 0, [timespan]::Zero).ToUnixTimeSeconds()
-
-
-[System.DateTimeOffset]::new(1970, 1, 1, 0, 0, 0, [timespan]::Zero)
-
-[System.DateTimeOffset]((Get-Date 1970-01-01).ToUniversalTime())
-
-
-Get-Date 1970-01-01 -UFormat %s
-
-Get-Date 2024-03-15 -UFormat %s
-
-
-
-
-# ----------------------------------------------------------------------
-
-$table = $result.optionChain.result[0].options[0].puts
-
-$json = @{
-    chart = @{
-        type = 'line'
-        data = @{
-            
-            labels = $table | ForEach-Object strike
-
-            datasets = @(
-
-                @{ label = 'SPY';       data = $table | ForEach-Object lastPrice; pointRadius = 2; }
-
-            )
-        }
-        options = @{
-            
-            title = @{ display = $true; text = 'Options Chain' }
-
-            scales = @{ }
-        }
-    }
-} | ConvertTo-Json -Depth 100
-
-$result = Invoke-RestMethod -Method Post -Uri 'https://quickchart.io/chart/create' -Body $json -ContentType 'application/json'
-
-# Start-Process $result.url
-
-$id = ([System.Uri] $result.url).Segments[-1]
-
-Start-Process ('https://quickchart.io/chart-maker/view/{0}' -f $id)
-# ----------------------------------------------------------------------
-
-$table = $result.optionChain.result[0].options[0].puts
-
-
-
-$table | ForEach-Object { @{ x = $_.strike; y = $_.lastPrice } }
-
-
-$json = @{
-    chart = @{
-        type = 'line'
-        data = @{
-            
-            # labels = $table | ForEach-Object strike
-
-            datasets = @(
-
-                # @{ label = 'SPY';       data = $table | ForEach-Object lastPrice; pointRadius = 2; }
-
-                @{ 
-                    label = 'SPY'
-                    
-                    # data = $table | ForEach-Object lastPrice
-
-                    data = $table | ForEach-Object { @{ x = $_.strike; y = $_.lastPrice } }
-                    
-                    pointRadius = 2
-                }
-
-            )
-        }
-        options = @{
-            
-            title = @{ display = $true; text = 'Options Chain' }
-
-            scales = @{ 
-                xAxes = @(@{ type = 'linear'})
-                yAxes = @(@{ type = 'linear'})
-            }
-        }
-    }
-} | ConvertTo-Json -Depth 100
-
-$result = Invoke-RestMethod -Method Post -Uri 'https://quickchart.io/chart/create' -Body $json -ContentType 'application/json'
-
-# Start-Process $result.url
-
-$id = ([System.Uri] $result.url).Segments[-1]
-
-Start-Process ('https://quickchart.io/chart-maker/view/{0}' -f $id)
-
-
-
-
-
-
-
-# ----------------------------------------------------------------------
-# linear axis
 # ----------------------------------------------------------------------
 
 function create-dataset-puts ($result)
 {
 
-    $symbol = $result.optionChain.result[0].underlyingSymbol
+    # $symbol = $result.optionChain.result[0].underlyingSymbol
     
     $seconds = $result.optionChain.result[0].options[0].expirationDate
 
@@ -237,8 +64,6 @@ function create-dataset-puts ($result)
     $dte = [math]::Ceiling(((Get-Date $date) - (Get-Date)).TotalDays)   
 
     @{
-        # label = $date
-
         label = "$date ${dte}d"
 
         data = $result.optionChain.result[0].options[0].puts | ForEach-Object { @{ x = $_.strike; y = $_.lastPrice } }
@@ -250,57 +75,17 @@ function create-dataset-puts ($result)
 }
 # ----------------------------------------------------------------------
 
+# $chains = chart-puts     SPY 2023-05-19, 2023-06-16, 2023-07-21, 2024-03-15, 2024-12-20
+
+# $symbol = 'SPY'
+
+# $expirations = '2023-05-19 2023-06-16 2023-07-21 2024-03-15 2024-12-20' -split ' '
+
+# $date = $expirations[0]
+
+# $chain = get-options-chain $symbol $date
 
 
-$json = @{
-    chart = @{
-        type = 'line'
-        data = @{            
-            datasets = @(
-                create-dataset-puts $result_0
-                create-dataset-puts $result_1
-                create-dataset-puts $result_2
-            )
-        }
-        options = @{
-            
-            title = @{ display = $true; text = 'Options Chain' }
-
-            scales = @{ 
-                xAxes = @(@{ type = 'linear'})
-                yAxes = @(@{ type = 'linear'})
-            }
-
-            annotation = @{
-
-                annotations = @(
-
-                    @{
-                        type = 'line'
-                        mode = 'vertical'
-                        value = $result_0.optionChain.result[0].quote.regularMarketPrice
-                        scaleID = 'X1'
-                        borderColor = 'red'
-                        borderWidth = 1
-                        label = @{ }
-                    }
-                )
-            }
-
-            plugins = @{ datalabels = @{ display = $false } }
-
-        }
-    }
-} | ConvertTo-Json -Depth 100
-
-$result = Invoke-RestMethod -Method Post -Uri 'https://quickchart.io/chart/create' -Body $json -ContentType 'application/json'
-
-# Start-Process $result.url
-
-$id = ([System.Uri] $result.url).Segments[-1]
-
-Start-Process ('https://quickchart.io/chart-maker/view/{0}' -f $id)
-# ----------------------------------------------------------------------
 function chart-puts ($symbol, $expirations)
 {
     $chains = foreach ($date in $expirations)
@@ -359,56 +144,6 @@ function chart-puts ($symbol, $expirations)
     
     Start-Process ('https://quickchart.io/chart-maker/view/{0}' -f $id)       
 }
-
-
-
-exit
-
-# ----------------------------------------------------------------------
-
-(Get-Date 2024-12-20) - (Get-Date)
-
-
-
-$chains = chart-puts SPY 2023-05-19, 2023-06-16, 2023-07-21, 2024-03-15, 2024-12-20
-
-
-$chains = chart-puts TSLA 2023-05-19, 2023-06-16, 2024-03-15
-
-$chains = chart-puts QQQ 2023-05-19, 2023-06-16, 2024-03-15
-
-
-$symbol = 'SPY'
-$expirations = '2023-05-19','2023-06-16','2024-03-15'
-
-$chains[0].optionChain.result[0].quote.regularMarketPrice
-
-
-# ----------------------------------------------------------------------
-$chain = get-options-chain SPY 2024-03-15
-
-$chain.optionChain.result[0].options[0].puts | ft *
-
-
-
-# $row = $chain.optionChain.result[0].options[0].puts[1]
-
-
-function percent ($a, $b) { ($b - $a) / $a }
-
-
-$prev = $chain.optionChain.result[0].options[0].puts[0]
-
-foreach ($row in $chain.optionChain.result[0].options[0].puts | Select-Object -Skip 1)
-{
-    $row | Add-Member -MemberType NoteProperty -Name pct_chg_per_dol `
-        -Value ([math]::Round(( (percent $prev.lastPrice $row.lastPrice) / ($row.strike - $prev.strike) ), 5)) `
-        -Force
-}
-
-
-
-
 # ----------------------------------------------------------------------
 # plot percent change in price
 # ----------------------------------------------------------------------
@@ -443,9 +178,7 @@ function create-dataset-puts-chg ($result)
         pointRadius = $result.optionChain.result[0].options[0].puts | ForEach-Object { [math]::Min($_.oi_pct * 100, 10) }
     }
 }
-
-
-
+# ----------------------------------------------------------------------
 function chart-puts-chg ($symbol, $expirations)
 {
     $chains = foreach ($date in $expirations)
@@ -504,6 +237,69 @@ function chart-puts-chg ($symbol, $expirations)
     
     Start-Process ('https://quickchart.io/chart-maker/view/{0}' -f $id)       
 }
+# ----------------------------------------------------------------------
+exit
+# ----------------------------------------------------------------------
+
+
+$chains = chart-puts     SPY 2023-05-19, 2023-06-16, 2023-07-21, 2024-03-15, 2024-12-20
+$chains = chart-puts-chg SPY 2023-05-19, 2023-06-16, 2023-07-21, 2024-03-15, 2024-12-20
+
+
+
+
+
+
+
+
+
+
+(Get-Date 2024-12-20) - (Get-Date)
+
+
+
+$chains = chart-puts SPY 2023-05-19, 2023-06-16, 2023-07-21, 2024-03-15, 2024-12-20
+
+
+$chains = chart-puts TSLA 2023-05-19, 2023-06-16, 2024-03-15
+
+$chains = chart-puts QQQ 2023-05-19, 2023-06-16, 2024-03-15
+
+
+$symbol = 'SPY'
+$expirations = '2023-05-19','2023-06-16','2024-03-15'
+
+$chains[0].optionChain.result[0].quote.regularMarketPrice
+
+
+# ----------------------------------------------------------------------
+$chain = get-options-chain SPY 2024-03-15
+
+$chain.optionChain.result[0].options[0].puts | ft *
+
+
+
+# $row = $chain.optionChain.result[0].options[0].puts[1]
+
+
+
+
+
+$prev = $chain.optionChain.result[0].options[0].puts[0]
+
+foreach ($row in $chain.optionChain.result[0].options[0].puts | Select-Object -Skip 1)
+{
+    $row | Add-Member -MemberType NoteProperty -Name pct_chg_per_dol `
+        -Value ([math]::Round(( (percent $prev.lastPrice $row.lastPrice) / ($row.strike - $prev.strike) ), 5)) `
+        -Force
+}
+
+
+
+
+
+
+
 
 
 
